81f604aa695ebd86456439dab719fe4cf1426256
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2024-10-15 20:59:25 -0300

    Add files via upload

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function MovieCard({ id, title, backdrop_path }) {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <img src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`}/>
+            <Link to={`/movies/${id}`}>Saber mais</Link>
+        </div>
+    )
+
+}
