635c11721c863743ce22aff3240319817216a8d0
acquesta <lorenzzo.acquesta@gmail.com>
2024-10-16 22:30:00 -0300

    criando pagina detalhe

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 export default function MovieDetailPage(){
+
+    const { movieId } = useParams()
+
+    const [filme, setFilme] = useState([])
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/933260?api_key=be713c0f3820693d5b8eb83566bbe6cc&language=pt-br`)
+        .then(results => results.json())
+        .then(data => {
+            setFilme(data)
+        })
+        .catch(error => console.log(error))
+        .finally(() => console.log("Acabou"))
+    }, [])
+
+
     return(
         <>
         <h1>Movie Detail Page</h1>
+        <h2>{filme.title}</h2>
         {/* Exibe detalhes de um filme específico. */}
         </>
     )
