6a259f49b77783cc47059e2e0ed17db9aa909f43
acquesta <lorenzzo.acquesta@gmail.com>
2024-10-23 22:14:10 -0300

    arrumando erros

--- src/components/Cartaz.jsx
+++ src/components/Cartaz.jsx
@@ -39,11 +39,11 @@ function Cartaz() {
         <div>
             <h2 className="text-3xl text-center my-5">Filmes em cartaz</h2>
             <div>
-                <div className="flex flex-col justify-center pl-10 h-[70vh] w-full bg-cover bg-center" style={{backgroundImage:`url(https://image.tmdb.org/t/p/w1280${focoCartaz?.backdrop_path})`}} alt="" >
-                    <h2 className="text-5xl">{focoCartaz?.title}</h2>
-                    <Link to={`/movies/${focoCartaz?.id}`} className="w-[10vw] my-5 px-3 py-1 border-2 border-white rounded-xl text-center">Ver mais</Link>
+                <div className="flex flex-col justify-center pl-10 h-[50vw] w-full bg-cover bg-center" style={{backgroundImage:`url(https://image.tmdb.org/t/p/w1280${focoCartaz?.backdrop_path})`}} alt="" >
+                    <h2 className="text-xl sm:text-5xl">{focoCartaz?.title}</h2>
+                    <Link to={`/movies/${focoCartaz?.id}`} className="w-[15vw] my-5 px-3 py-1 border-2 border-white rounded-xl text-center">Ver mais</Link>
                 </div>
-                <div ref={scrollRef} className="flex gap-5 ml-5 mt-[-10rem] overflow-scroll overflow-y-hidden">
+                <div ref={scrollRef} className="flex gap-5 px-5 mt-[-10rem] overflow-scroll overflow-y-hidden">
                     {cartaz.map(filme => (
                         <div key={filme.id} className="w-[90vw] my-10 mx-auto gap-2 cursor-pointer">
                             <img onClick={() => trocaCartaz(filme)} className="min-w-[10vw] min-h-[10vw] rounded-xl" src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`} alt="" />
