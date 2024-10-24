582790c80dfe583acb5924f8eee2cd4c96d4b6d2
acquesta <lorenzzo.acquesta@gmail.com>
2024-10-24 10:16:27 -0300

    movie card responsivo

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -19,7 +19,7 @@ export default function CardContainer({ titulo, children }) {
 
     return (
         <div className="relative my-5">
-            <h1 className="text-3xl mx-10">{titulo}</h1>
+            <h1 className="text-xl mx-10 md:text-3xl">{titulo}</h1>
              
             <div className={`${children.length <= 5 ? 'hidden' : ''} flex items-center z-10 h-full my-5 pr-4 bg-gradient-to-r from-transparent to-[#080808] absolute right-0`}>
                 <button onClick={() => scrollDireita('left')} className="max-md:hidden z-10 w-12 h-12 bg-teal-500 rounded-full text-center">
