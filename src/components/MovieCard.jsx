0b4a2fdebf9afe59affa24b28f8ad139abe17f20
acquesta <lorenzzo.acquesta@gmail.com>
2024-10-25 08:29:11 -0300

    responsivo movieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,8 +5,8 @@ export default function MovieCard({ id, poster_path }) {
     return (
         <>
             <Link className="my-3 rounded-xl text-black font-bold" to={`/movies/${id}`}>
-                <div className="cursor-pointer ">
-                    <img className="md:w-[10vw] min-w-[20vw] hover:scale-105 transition-all rounded-xl" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="imagem não encontrada"/>
+                <div className="cursor-pointer md:w-[20vw]">
+                    <img className="md:min-w-[10vw] min-w-[35vw] hover:scale-105 transition-all rounded-xl" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="imagem não encontrada"/>
                 </div>
             </Link>
         </>
